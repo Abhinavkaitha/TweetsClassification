73744024e649a76592c72d95bb01b8e215e3efad
ABHINAV REDDY KAITHA <kaitha.abhi@gmail.com>
2019-01-02 19:07:25 -0500

    Add files via upload

--- geolocate.py
+++ geolocate.py
@@ -1,141 +1,445 @@
-#!/usr/bin/env python
-#Report given in Readme File
-import sys
+#!/usr/bin/env python3
+#THIS CODE TAKES 55 SECONDS ON BURROW FOR 32000 TWEETS
+# ---------------------------------------------------------------------------
+# 1 VocabuDic <-- all distinct words present in 32000 tweets
+# 2 Calculated the required P(location) and P(word|location) 
+#       n <-- total number of words in tweets from a particular location
+#       for each word Wk in Vocabulary
+#               nk <-- number of times a word occurs in a tweet from a particular place
+#               P(Word|location) <-- (nk + 1)/(n + len(VocabDic))
+# 3 p(location/tweet)=p(word1/location)p(word2/location)....p(location)
+# ---------------------------------------------------------------------------
+import pandas as pd
 import re
-from math import log
-def readfile(infile):
-    global train_tweet_count
-    word_dict = {}    
-    city_dict={}
-    with open(infile, 'r') as f:
-        for line in f:
-            train_tweet_count=train_tweet_count+1
-            city, tweet = line.split(' ', 1) # Separate city and tweet
-            if city not in city_dict:
-                city_dict[city]={}
-                city_dict[city]['Count_Words']=0
-                city_dict[city]['Count_Tweets']=1
-            else:
-                city_dict[city]['Count_Tweets'] = city_dict[city]['Count_Tweets'] + 1            
-            word_list = get_words(tweet) # Get only the lowercase words
-            city_dict[city]['Count_Words']=city_dict[city]['Count_Words'] + len(word_list)            
-            fill_dictionary(city, word_list, word_dict) # Fill in the word_dict            
-    return word_dict,  city_dict
+import numpy as np
+from collections import Counter
+import heapq
+import sys
+
+# In[2]:
+train=sys.argv[1]
+test=sys.argv[2]
+output=sys.argv[3]
+#storing each line from train file in tweets train
+tweetsTrain=[]
+with open(train) as file:
+    for line in file:
+        tweetsTrain.append(line)
 
 
-def readtestfile(infile,train_words,cities):
-    #global train_tweet_count
-    len_train_words = len(train_words)    
-    with open(infile, 'r') as f:
-        test_tweet={}
-        count = 0
-        for line in f:
-            count += 1
-            city, tweet = line.split(' ', 1) # Separate city and tweet                        
-            word_list = get_words(tweet) # Get only the lowercase words
-            for i in list(range(0,len(word_list))):
-                if len(word_list[i]) > 5:
-                    word_list[i] = word_list[i][0:5]
-            test_tweet[count] = {}
-            test_tweet[count]['Actual_City'] = city
-            test_tweet[count]['Predicted_City'] = ''
-            test_tweet[count]['Tweet'] = tweet
-            test_tweet[count]['Cities_Probability'] = {}
-            max_log_probability = -float("inf")
-            max_probable_city = ''
-            for city in cities:
-               city_word_count = cities[city]['Count_Words'] 
-               test_tweet[count]['Cities_Probability'][city] = log( float(cities[city]['Count_Tweets']) / float(train_tweet_count)) 
-               for word in word_list:
-                   try:                  
-                       test_tweet[count]['Cities_Probability'][city] = test_tweet[count]['Cities_Probability'][city] + log(float(train_words[word]['Cities'][city]+1) / float(city_word_count + len_train_words ))
-                   except:
-                       continue
-               if test_tweet[count]['Cities_Probability'][city] > max_log_probability:
-                   max_log_probability = test_tweet[count]['Cities_Probability'][city]
-                   max_probable_city = city
-                   test_tweet[count]['Predicted_City'] = max_probable_city
-                                              
-    return test_tweet
+# In[3]:
 
 
-
-def fill_dictionary(city, word_list, word_dict):
-    for word in word_list:
-        if word in stop_words or word.isdigit()==True or len(word)<2:
-            continue
-        if len(word)>5:
-            word = word[0:5]
-        if word in word_dict:
-            word_dict[word]['Total_Count'] +=1
-            if city in word_dict[word]['Cities']:
-                word_dict[word]['Cities'][city] += 1
-            else:
-                word_dict[word]['Cities'][city] = 1
-        else:            
-            word_dict[word] = {}
-            word_dict[word]['Cities']={}
-            word_dict[word]['Cities'][city] = 1
-            word_dict[word]['Total_Count']=1
+# storing each line from test file in tweets test
+tweetsTest=[]
+with open(test) as file:
+    for line in file:
+        tweetsTest.append(line)
 
 
+# I have splitted each line with the first space
+# 
 
-def get_words(tweet):
-    return [word.lower() for word in re.split(r"[\W_]", tweet) if word != '']
-
-#Main Start
-stop_words = ['a','an','and','are','the','as', 'at', 'be' ,'by' ,'us','it','too','she' ,'for', 'from', 'has','he', 'in', 'yes','is', 'its', 'of', 'on', 'that', 'to', 'was', 'were', 'will', 'with','my','you','mine','yours','we','can','this','our','because','him','his','her']    
-training_file = sys.argv[1]
-testing_file = sys.argv[2]
-output_file = sys.argv[3]
-train_tweet_count=0
-#Making Words, cities and hashtag dic
-words, cities = readfile(training_file)
-
-#Filling up 0 for cities not there for word
-for word in words:
-    for city in cities:
-        if city not in words[word]['Cities']:
-            words[word]['Cities'][city]=0
-    
-
-#Making list of Train - words
-train_words = words            
-for word in list(train_words.keys()):
-    if train_words[word]['Total_Count']==1:
-        del(train_words[word])
-#test file
-#Laplace smoothing done to all just to be on safe side
-test_tweet = readtestfile(testing_file,train_words,cities)
-
-#Accuracy
-count_correct=0
-for key in test_tweet:
-    if test_tweet[key]['Actual_City'] == test_tweet[key]['Predicted_City']:
-        count_correct = count_correct + 1
-
-accuracy = float(count_correct) / float(len(test_tweet))
+# In[4]:
 
 
-#Printing Top 5 words associated with each location
-for city in cities:
-    city_words_array=[]
-    city_word_count_array=[]
-    top_5_words_city = []
-    i = 1    
-    for word in train_words:                
-        city_words_array.append(word)
-        city_word_count_array.append(train_words[word]['Cities'][city])
-    while i <= 5:        
-        index = city_word_count_array.index(max(city_word_count_array))
-        top_5_words_city.append(city_words_array.pop(index))
-        del(city_word_count_array[index])
-        i += 1
-    print( city + " " + " ".join([word for word in top_5_words_city]))        
-    
-    
-#Writing to Output
-f = open(output_file , "a+")
-for index in test_tweet:
-   f.write( test_tweet[index]['Predicted_City'] + " " + test_tweet[index]['Actual_City'] + " " +  test_tweet[index]['Tweet'])
+# splliting and separating the places and tweets for both test and train file
+SplittweetsTrain=[]
+for i in range(len(tweetsTrain)):
+    SplittweetsTrain.append(tweetsTrain[i].split(" ",1))
+SplittweetsTest=[]
+for i in range(len(tweetsTest)):
+    SplittweetsTest.append(tweetsTest[i].split(" ",1))
+
+
+# created a separate list for places and tweets
+
+# In[5]:
+
+
+places=[]
+tweets=[]
+for i in SplittweetsTrain:
+    places.append(i[0])
+    tweets.append(i[1])
+
+
+# In[6]:
+
+
+Tplaces=[]
+Ttweets=[]
+for i in SplittweetsTest:
+    Tplaces.append(i[0])
+    Ttweets.append(i[1])
+
+
+# In[7]:
+
+
+#created dictionaries to store places and tweets
+trainDic={}
+trainDic["places"]=places
+trainDic["tweets"]=tweets
+
+
+# In[8]:
+
+
+testDic={}
+testDic["places"]=Tplaces
+testDic["tweets"]=Ttweets
+
+
+# Created a list of tweets by splitting them
+
+# In[9]:
+
+
+SplitTweets=[]
+for i in trainDic["tweets"]:
+    SplitTweets.append(i.split())
+
+
+# In[10]:
+
+
+TSplitTweets=[]
+for i in testDic["tweets"]:
+    TSplitTweets.append(i.split())
+
+
+# List of stop words
+
+# In[11]:
+
+
+# I have collected  these stop words from NLTK package, https://www.ranks.nl/stopwords, from this website. I took the union of both of them
+# and also I have found some words like job,jobs,careerarc,day, latest,amp,click which are in top 5 among all the places
+#So, they wont play any major role in decision making. So I removed them
+stop_words = ["city",'lol','good','st','know','got',"pm",'want',"click","amp",'latest', 'day',"careerarc","hiring","job","jobs",'enough',"im",'eg', 'thatll', 'something', 'often', 'nobody', 'more', 'seems', 're', 'therefore', 'until', 'well', 'wasnt', 'your', 'other', 'throughout', 'whereupon', 'thence', 'z', 'may', 'each', 'now', 'someone', 'become', 'between', 'amoungst', 'what', 'during', 'won', 'just', 'whereafter', 'another', 'go', 'out', 'beside', 'find', 'itself', 'x', 'shouldnt', 'few', 'hasnt', 'still', 'please', 'mightn', 'amount', 'shant', 'both', 'everything', 'themselves', 'youll', 'most', 'therein', 'hadnt', 'ltd', 'over', 'part', 'our', 'whoever', 'sometime', 'next', 'herself', 'r', 'becoming', 'whence', 'f', 'anything', 'g', 'afterwards', 'whatever', 'almost', 'cannot', 'b', 'thereafter', 'isnt', 'didnt', 'd', 'none', 'ourselves', 'so', 'her', 'herein', 'either', 'of', 'anywhere', 'have', 'need', 'and', 'up', 'at', 'amongst', 'she', 'whether', 'already', 'yours', 'wherein', 'mightnt', 'had', 'ma', 'many', 've', 'h', 'was', 'sometimes', 'where', 'except', 'everyone', 'meanwhile', 'y', 'off', 'n', 'havent', "should've", 'his', 'also', 'here', 'once', 'done', 'how', 'mine', 'seem', 'to', 'yourself', 'this', 'together', 'cant', 'above', 'does', 'through', 'ever', 'alone', 'w', 'others', 'in', 'these', 'thru', 'within', 'although', 'he', 'behind', 'us', 'even', 'such', 'there', 'll', 'it', 'than', 'whose', 'against', 'mustnt', 'moreover', 'beyond', 'somehow', 'for', 'couldnt', 'am', 'u', 'one', 'are', 'dont', 'you', 'sincere', 't', 'some', 'describe', 'my', 'since', 'de', 'has', 'without', 'could', 'further', 'before', 'ours', 'shan', 'wasn', 'mostly', 'myself', 'might', 'any', 'not', 'v', 'after', 'them', 'thus', 'wouldnt', 'everywhere', 'their', 'p', 'its', 'would', 'much', 's', 'somewhere', 'all', 'neither', 'then', 'be', 'or', 'own', 'take', 'but', 'too', 'anyway', 'else', 'same', 'whenever', 'while', 'if', 'yet', 'ain', 'did', 'having', 'anyhow', 'besides', 'made', 'nor', 'm', 'only', 'otherwise', 'seemed', 'among', 'that', 'whereas', 'which', 'a', 'however', 'across', 'those', 'less', 'though', 'because', 'wherever', 'i', 'about', 'being', 'rather', 'under', 'when', 'etc', 'four', 'nevertheless', 'the', 'thereby', 'with', 'below', 'c', "hasn't", 'thereupon', 'yourselves', 'him', 'do', 'youd', 'we', 'must', 'hers', 'get', 'perhaps', 'anyone', 'towards', 'doesnt', 'wont', 'always', 'found', 'hereupon', 'indeed', 'keep', 'noone', 'were', 'an', 'nowhere', 'from', 'every', 'nothing', 'no', 'onto', 'put', 'hereby', 'who', 'name', 'is', 'doing', 'k', 'youre', 'whereby', 'never', 'very', 'l', 'theirs', 'why', 'arent', 'again', 'becomes', 'give', 'himself', 'j', 'been', 'see', 'will', 'werent', 'e', 'they', 'youve', 'by', 'q', 'hence', 'on', 'o', 'neednt', 'seeming', 'along', 'can',"city" 'should', 'least', 'toward', 'upon', 'ie', 'into', 'became', 'hereafter', 'me', 'namely', 'down', 'whom', 'as']
+
+
+# Removong the special charecters,punctuations and numbers
+
+# In[12]:
+
+
+newSplitTweets=[]
+x=[]
+for i in SplitTweets:
+    for j in i:
+        x.append(re.sub('[^a-zA-Z \n\.]', '', j))
+    newSplitTweets.append(" ".join(x).lower())
+    x=[]
+
+
+# In[13]:
+
+
+TnewSplitTweets=[]
+x=[]
+for i in TSplitTweets:
+    for j in i:
+        x.append(re.sub('[^a-zA-Z \n\.]', '', j))
+    TnewSplitTweets.append(" ".join(x).lower())
+    x=[]
+
+
+# In[14]:
+
+
+trainDic["clean"]=newSplitTweets
+
+
+# In[15]:
+
+
+testDic["clean"]=TnewSplitTweets
+
+
+# Again, splitting the clean tweets to remove the stop words
+
+# In[16]:
+
+
+SplitTweets1=[]
+for i in trainDic["clean"]:
+    SplitTweets1.append(i.split())
+
+
+# In[17]:
+
+
+TSplitTweets1=[]
+for i in testDic["clean"]:
+    TSplitTweets1.append(i.split())
+
+
+# Removing the stop words and adding them to dictionary
+
+# In[18]:
+
+
+newSplitTweets1=[]
+x1=[]
+for i in SplitTweets1:
+    for j in i:
+        if j not in stop_words:
+            x1.append(j)
+    newSplitTweets1.append(" ".join(x1))
+    x1=[]
+
+
+# In[19]:
+
+
+TnewSplitTweets1=[]
+x1=[]
+for i in TSplitTweets1:
+    for j in i:
+        if j not in stop_words:
+            x1.append(j)
+    TnewSplitTweets1.append(" ".join(x1))
+    x1=[]
+
+
+# In[20]:
+
+
+trainDic["clean1"]=newSplitTweets1
+
+
+# In[21]:
+
+
+testDic["clean1"]=TnewSplitTweets1
+
+
+# creating a list of all the words present in the tweets
+
+# In[22]:
+
+
+vocab=[]
+for i in trainDic["clean1"]:
+    vocab.extend(i.split())
+
+
+# Removing strings attachements like ".", "..." etc
+
+# In[23]:
+
+
+vocabulary=vocab
+for i in range(len(vocabulary)):
+    vocabulary[i]=vocabulary[i].replace(".","").replace(" ","")
+
+
+# In[24]:
+
+
+vocabDic={}
+vocabDic=Counter(vocabulary)
+
+
+# In[25]:
+
+
+df=pd.DataFrame(trainDic)
+
+
+# In[26]:
+
+
+# this function returns a dictionaty with all the clean words and their count present in all the tweets from place
+#similar to the previous operations
+def WordCount(place):
+    i=0
+    index=[]
+    for b in trainDic["places"]:
+        if b==place:
+            index.append(i)
+        i+=1
+    PlaceVocab=[]
+    for i in list(map(lambda x : trainDic["clean1"][x],index)):
+        PlaceVocab.extend(i.split())
+    PlaceVocabulary=PlaceVocab
+    for i in range(len(PlaceVocabulary)):
+        PlaceVocabulary[i]=PlaceVocabulary[i].replace(".","").replace(" ","")
+    PlaceVocabDic={}
+    PlaceVocabDic=Counter(PlaceVocabulary)
+    del PlaceVocabDic[""]
+    return PlaceVocabDic
+
+
+# In[27]:
+
+
+Places=set(df["places"])
+
+
+# In[28]:
+
+
+Tdf=pd.DataFrame(testDic)
+
+
+# In[29]:
+
+
+TPlaces=set(Tdf["places"])
+
+
+# In[30]:
+
+
+#this masterdic is a nested dictionay which consists of all the words present in all the places and their count
+MasterDic={}
+for i in Places:
+    MasterDic[i]=WordCount(i)
+
+
+# In[31]:
+
+
+#This p dictionary will consists of p(word/location) for all the word in the vocabDic
+#If a particulat word doesn't have this value then it is substitute with the least positive value
+P={}
+MasterP={}
+for i in vocabDic:
+    for j in Places:
+        try:
+            P[j]=(MasterDic[j][i]+1)/(sum(MasterDic[j].values())+len(vocabDic))
+        except KeyError:
+            P[j]=np.finfo(np.float64).eps
+    MasterP[i]=P
+    P={}
+
+
+# In[32]:
+
+
+#Calculated the p(location)
+PlaceP={}
+for i in Places:
+    PlaceP[i]=(sum(df["places"]==i))/len(trainDic["places"])
+
+
+# In[33]:
+
+
+testDf=pd.DataFrame(testDic)
+
+
+# In[34]:
+
+
+for i in range(len(testDf)):
+    testDf["clean1"][i]=" ".join(testDf["clean1"][i].split("."))
+
+
+# In[35]:
+
+
+#consists of all the clean words in each line of test file 
+RowDic={}
+for i in range(len(testDf)):
+    RowDic[i]=testDf["clean1"][i].split()
+
+
+# In[36]:
+
+
+#Initiated a dictionary to store all p(location/word) i.e, probrability that a tweet is from a particular place
+RowWordDic={}
+xps={}
+for i in RowDic:
+    for p in Places:
+        xps[p]=0
+    RowWordDic[i]=xps
+    xps={}
+
+
+# In[37]:
+
+
+#Calculated p(location/word) and multiplied them for a particular tweet
+p=1
+for j in range(len(RowDic)):
+    for k in Places:
+        for i in RowDic[j]:
+            try:
+                p=p*MasterP[i][k]
+            except KeyError:
+                continue
+        RowWordDic[j][k]=p*PlaceP[k]
+        p=1
+
+
+# In[38]:
+
+
+#Initiated a dictionary to store all the maximum probabilities from a place
+Ans={}
+for i in range(len(RowDic)):
+    Ans[i]=0
+
+
+# In[39]:
+
+
+# took the maximum value from a particular tweet and stored it in qwe
+#Then took the first value(in case if there are multiple maximum values)
+qwe=[]
+for i in range(len(RowWordDic)):
+    for a,b in RowWordDic[i].items():
+        if b==max(RowWordDic[i].values()):
+            qwe.append(a)
+    Ans[i]=qwe[0]
+    qwe=[]
+
+
+# In[40]:
+
+
+with open(output,"w") as f:
+    output=[]
+    for i in range(len(tweetsTest)):
+        tlist=tweetsTest[i].split()
+        tlist.insert(0,Ans[i])
+        output.append((" ".join(tlist)))
+    f.writelines(["%s\n" % item  for item in output])
 f.close()
+
+
+# In[42]:
+
+
+#Printing the top words from each place p(location/word)
+top={}
+test=[]
+value=0
+temp={}
+for j in Places:
+    for i in vocabDic:
+        try:
+            if MasterDic[j][i]!=1:
+                value=MasterP[i][j]*PlaceP[j]
+        except KeyError:
+            continue
+        temp[i]=value
+    top[j]=temp
+    temp={}
+
+
+# In[43]:
+
+
+#I DID NOT GET THE WORDS LIKE "click","amp",'latest', 'day',"careerarc","hiring","job","jobs",'enough',"im" BEACUSE I REMOVED
+#THEM TO INCREASE THE EFFICIENCY
+print("Top 5 words in each location")
+for i in Places:
+    print(i)
+    print(heapq.nlargest(5, top[i], key=top[i].get))
+
